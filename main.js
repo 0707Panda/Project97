08287ee1fab432905000027097cc3b4519c52c8a
0707Panda <90808764+0707Panda@users.noreply.github.com>
2022-08-04 22:30:46 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+function mathbtn(){
+    window.location="math.html";
+}
+function englishbtn(){
+    window.location="english.html";
+}
+function socialbtn(){
+    window.location="social.html";
+h}
+function sciencebtn(){
+    window.location="science.html";
+}
